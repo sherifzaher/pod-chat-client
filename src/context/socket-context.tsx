ff88eafc3a4d1a2439abb4d359f37560ecccbf39
sherifzaher <sherifashrafmedia@gmail.com>
2023-07-02 06:16:51 +0300

    add withCredentials to socket io

--- src/context/socket-context.tsx
+++ src/context/socket-context.tsx
@@ -1,7 +1,9 @@
 import { io } from 'socket.io-client';
 import { createContext, useContext } from 'react';
 
-export const socket = io(process.env.REACT_APP_WEBSOCKET_URL!);
+export const socket = io(process.env.REACT_APP_WEBSOCKET_URL!, {
+  withCredentials: true,
+});
 
 export const SocketContext = createContext(socket);
 
