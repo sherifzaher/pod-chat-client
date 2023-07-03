a36810139ddc4387f8a46c8ff01cfbf345b8f7cf
sherifzaher <sherifashrafmedia@gmail.com>
2023-07-03 08:40:21 +0300

    move socket event listener to the conversation page

--- src/pages/conversation-channel-page.tsx
+++ src/pages/conversation-channel-page.tsx
@@ -2,38 +2,21 @@ import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
-import { useSocketContext } from '../context/socket-context';
-import {addMessage, fetchMessagesThunk} from '../store/slices/messages-slice';
+import {fetchMessagesThunk} from '../store/slices/messages-slice';
 import { AppDispatch } from '../store';
 
 import MessagePanel from '../components/messages/message-panel';
 
 import { ConversationChannelPageStyle } from '../utils/styles';
-import {updateConversation} from "../store/slices/conversation-slice";
 
 function ConversationChannelPage() {
   const { id } = useParams<{ id: string }>();
   const dispatch = useDispatch<AppDispatch>();
-  const socket = useSocketContext();
 
   useEffect(() => {
     if (!id) return;
     dispatch(fetchMessagesThunk(Number(id)));
   }, [id]);
-
-  useEffect(() => {
-    socket.on('connected', () => console.log('connected'));
-    socket.on('onMessage', (payload: MessageEventPayload) => {
-      const { conversation } = payload;
-      dispatch(addMessage(payload));
-      dispatch(updateConversation(conversation));
-    });
-
-    return () => {
-      socket.off('connected');
-      socket.off('onMessage');
-    };
-  }, [socket, dispatch, id]);
 
   return (
     <ConversationChannelPageStyle>
--- src/pages/conversation-page.tsx
+++ src/pages/conversation-page.tsx
@@ -2,18 +2,35 @@ import { useEffect } from 'react';
 import { Outlet, useParams } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { AppDispatch } from '../store';
-import { fetchConversationsThunk } from '../store/slices/conversation-slice';
+import {fetchConversationsThunk, updateConversation} from '../store/slices/conversation-slice';
 import { Page } from '../utils/styles';
 import ConversationSidebar from '../components/conversations/conversation-sidebar';
 import ConversationPanel from '../components/conversations/conversation-panel';
+import {addMessage} from "../store/slices/messages-slice";
+import {useSocketContext} from "../context/socket-context";
 
 function ConversationsPage() {
   const dispatch = useDispatch<AppDispatch>();
+  const socket = useSocketContext();
   const { id } = useParams();
 
   useEffect(() => {
     dispatch(fetchConversationsThunk());
   }, []);
+
+  useEffect(() => {
+    socket.on('connected', () => console.log('connected'));
+    socket.on('onMessage', (payload: MessageEventPayload) => {
+      const { conversation } = payload;
+      dispatch(addMessage(payload));
+      dispatch(updateConversation(conversation));
+    });
+
+    return () => {
+      socket.off('connected');
+      socket.off('onMessage');
+    };
+  }, [socket, dispatch, id]);
 
   return (
     <Page>
